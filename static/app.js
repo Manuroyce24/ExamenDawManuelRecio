a161eb660b06e605643d907f90230ea0fe7179cb
Manuel Recio Claros <manuroyce@outlook.com>
2022-12-19 09:49:19 +0100

    Primer commit de tabla+grid+modal

---
+++ static/app.js
@@ -0,0 +1,13 @@
+// nombre_del_alumno
+//
+function openModal() {
+    document.getElementById("login-modal").style.display = "block";
+    document.getElementById("modal-overlay").style.display = "block";
+}
+
+// Cerrar login modal
+function closeModal() {
+    document.getElementById("login-modal").style.display = "none";
+    document.getElementById("modal-overlay").style.display = "none";
+}
+// Completar JS aqui
